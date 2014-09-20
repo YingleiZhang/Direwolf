4d7e7e7a4cd3d93dd609160a3cf60e7b3ab09c89
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2014-09-20 18:59:58 -0400

    Setting up instance variables for user

--- direwolf/app/controllers/home_controller.rb
+++ direwolf/app/controllers/home_controller.rb
@@ -2,5 +2,10 @@ class HomeController < ApplicationController
   def show
   end
   def register
+    if session[:user_id]
+      if User.exists?(session[:user_id])
+        @user = UserController.new(session[:user_id])
+      end
+    end
   end
 end
