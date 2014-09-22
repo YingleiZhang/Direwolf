4300b4833a792686baa8bef2d0d31832237b2e28
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2014-09-22 00:55:10 -0400

    Adding user_is function in UserHelpre module which will help authenticate user

--- direwolf/app/helpers/users_helper.rb
+++ direwolf/app/helpers/users_helper.rb
@@ -1,2 +1,31 @@
 module UsersHelper
+  def user_is should_be
+    id = session[:user_id]
+    if User.exists?(id)
+      info = User.find(id)
+      should_be = :user
+      if Seeker.exists?(user_id: info.uid)
+        should_be = :seeker
+      else
+        if Employer.exists?(user_id: info.uid)
+          should_be = :employer
+        else
+          if Admin.exists?(user_id: info.uid)
+            should_be = :admin
+          else
+            should_be = nil
+          end
+        end
+      end
+    else
+      should_be = nil
+    end
+    if actually_is == should_be
+      return :true
+    else
+      return :false
+    end
+  end
+
 end
+
