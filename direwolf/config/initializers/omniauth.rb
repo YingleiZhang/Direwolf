f03f50b5bac4aaceac1eaaf1c532615bfa8e988d
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2014-09-27 04:37:16 -0400

    Adding new auth id as old one is used for heroku

--- direwolf/config/initializers/omniauth.rb
+++ direwolf/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '269123582486-kgk51jp5bvuu911nuac3tq5dk3oc5vad.apps.googleusercontent.com', '0CaMDmAGl859ldYuJKDVaRHj', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '269123582486-6v003oqpsu4lv1k6079bqoong4rjbaj3.apps.googleusercontent.com', 'YTR6nELkBFRHc2iENMvgVRxF', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
