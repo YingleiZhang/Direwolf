b44784d226c0441d934ce3dda763741239813bcd
David Zager <djzager@ncsu.edu>
2014-10-08 21:54:12 -0400

    Configuring mailers for production

--- direwolf/config/environments/production.rb
+++ direwolf/config/environments/production.rb
@@ -77,4 +77,16 @@ Direwolf::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'example.com',
+      user_name:            'direwolfapp.user1@gmail.com',
+      password:             'appAdmin',
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 end
